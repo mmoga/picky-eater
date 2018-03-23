bf4fd96d9be29623673436ad5f66956619ad758b
Matthew Mogavero <matt.moga@gmail.com>
2018-03-23 19:35:14 -0400

    installed leaflet and applied leaflet css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import Container from './Container';
 import './App.css';
 
 class App extends Component {
   render() {
+    // if (!this.props.loaded) {
+    //   return <div>Loading...</div>
+    // }
     return (
       <div>
-        TEST
+        <Container />
       </div>
     );
   }
 }
 
 export default App;
+
+
