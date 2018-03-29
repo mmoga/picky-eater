19dfd977a520832db98baad0c5f072d766a0fe8e
Matthew Mogavero <matt.moga@gmail.com>
2018-03-28 20:52:54 -0400

    set up basics for Choices and AntiChoices components

---
+++ client/src/AntiChoices.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// this component will host the restaurants
+// that have been disliked
+
+class AntiChoices extends Component {
+    render() {
+        return (
+            <div>
+                TEST
+            </div>
+        )
+    }
+}
+
+export default AntiChoices;
---
+++ client/src/Choices.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// this component will host the randomly drawn restaurants
+// that were either liked or yet unmarked
+
+class Choices extends Component {
+    render() {
+        return (
+            <div>
+                TEST
+            </div>
+        )
+    }
+}
+
+export default Choices;
