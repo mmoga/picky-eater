2c77fcd160d3fa1d13860bbf8c39d539e02694da
Matthew Mogavero <matt.moga@gmail.com>
2018-04-09 20:16:11 -0400

    added buttons with click events on popup

--- client/src/PickerPopup.js
+++ client/src/PickerPopup.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 
 class PickerPopup extends Component {
-    render() {
-        return (
-            <div className={`PickerPopup--container ${this.props.isShown ? "PickerPopup--container-shown" : "PickerPopup--container-hidden"}`}>
-                <h1>
-                    Popup launched!
-                </h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div
+        className={`PickerPopup--container ${
+          this.props.isShown
+            ? "PickerPopup--container-shown"
+            : "PickerPopup--container-hidden"
+        }`}
+      >
+        <div className="PickerPopup--title">
+          <h1>Popup launched!</h1>
+        </div>
+        <div className="PickerPopup--btns">
+          <button onClick={() => console.log("do it over!")}>
+            Eh... try it again
+          </button>
+          <button onClick={() => console.log("close!")}>Close</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default PickerPopup;
