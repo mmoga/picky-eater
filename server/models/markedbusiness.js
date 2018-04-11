0ded24bec972a6a22be7c216c8a31f201cce5710
Matthew Mogavero <matt.moga@gmail.com>
2018-04-10 20:04:47 -0400

    removed comment

--- server/models/markedbusiness.js
+++ server/models/markedbusiness.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const markedSchema = new Schema({
- // name: String, // I would add more info about this property
     name: {
         required: true,
         type: String,
